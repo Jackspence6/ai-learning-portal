c390d24427a3617d90f7975b29e276b1a56ada93
Jack Spence <jackspence123456@gmail.com>
2023-11-23 18:41:57 +0200

    Added properties etc. to Quizzes model.

---
+++ models/Quizzes.js
@@ -0,0 +1,47 @@
+// Imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Quizzes extends Model {}
+
+// Quizzes Model properties
+Quizzes.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		tutorial_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: "tutorials",
+				key: "id",
+			},
+		},
+		questions: {
+			type: DataTypes.JSON,
+			allowNull: false,
+		},
+		answers: {
+			type: DataTypes.JSON,
+			allowNull: false,
+		},
+		correct_answers: {
+			type: DataTypes.JSON,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		timestamps: true,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "quizzes",
+	}
+);
+
+// Exports
+module.exports = Quizzes;
