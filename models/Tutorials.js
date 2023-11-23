84b6de61d20d05f19b8c93c8bb6a60f815d21283
Jack Spence <jackspence123456@gmail.com>
2023-11-23 18:30:50 +0200

    Added properties etc. to Tutorials model.

---
+++ models/Tutorials.js
@@ -0,0 +1,49 @@
+// Imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Tutorials extends Model {}
+
+// Tutorials Model properties
+Tutorials.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		content: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		date_created: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW,
+		},
+		date_updated: {
+			type: DataTypes.DATE,
+			allowNull: true,
+		},
+	},
+	{
+		sequelize,
+		timestamps: true,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "tutorials",
+	}
+);
+
+// Exports
+module.exports = Tutorials;
