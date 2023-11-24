72372a902a1283b4771fab9294118abaae3eb74c
Jack Spence <jackspence123456@gmail.com>
2023-11-24 13:23:00 +0200

    Added Route to display homepage.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,6 +7,15 @@ const {
 	Quizzes,
 	ExampleCode,
 } = require("../models");
+require("./home-routes");
+
+// Route to display homepage
+router.get("/", (req, res) => {
+	res.render("homepage", {
+		logged_in: req.session.logged_in,
+		on_homepage: true,
+	});
+});
 
 // Exports
 module.exports = router;
