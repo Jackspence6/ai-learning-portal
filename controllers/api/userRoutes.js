3f022ce291c9c607046b535a26bca90b18dacb0a
Jack Spence <jackspence123456@gmail.com>
2023-11-24 15:42:33 +0200

    Added Route to Create a new User.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,5 +2,21 @@
 const router = require("express").Router();
 const { Users } = require("../../models");
 
+// Route to Create a new User
+router.post("/", async (req, res) => {
+	try {
+		const userData = await Users.create(req.body);
+
+		// Saving Session to cookies to persist login
+		req.session.save(() => {
+			req.session.user_id = userData.id;
+			req.session.logged_in = true;
+			res.status(200).json(userData);
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 // Exports
 module.exports = router;
