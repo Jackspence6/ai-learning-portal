e9e4d19deb77024c431afbb6b100680402b9b6df
Jack Spence <jackspence123456@gmail.com>
2023-11-25 17:50:25 +0200

    Added Route to GET single quiz by it's Id.

--- controllers/api/quizRoutes.js
+++ controllers/api/quizRoutes.js
@@ -23,5 +23,30 @@ router.get("/", async (req, res) => {
 	}
 });
 
+// Route to GET single quiz by it's Id
+router.get("/:id", async (req, res) => {
+	try {
+		const quizData = await Quizzes.findByPk(req.params.id);
+
+		// Checking if that Quiz exists
+		if (!quizData) {
+			res.status(404).json({ message: "No Quiz found with this Id!" });
+			return;
+		}
+
+		// Serializing data so the template can read it
+		const quizzes = quizData.get({ plain: true });
+
+		// Passing serialized data into the Handlebars template
+		res.render("quizzes", {
+			quizzes,
+			logged_in: req.session.logged_in,
+			on_singleQuiz: true,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 // Exports
 module.exports = router;
