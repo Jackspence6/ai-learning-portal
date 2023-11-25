f618ca012b4362666af9d01f1b6917c82cd6fee9
Jack Spence <jackspence123456@gmail.com>
2023-11-25 13:34:13 +0200

    Added part of Route to GET all Quizzes.

--- controllers/api/quizRoutes.js
+++ controllers/api/quizRoutes.js
@@ -2,5 +2,26 @@
 const router = require("express").Router();
 const { Quizzes } = require("../../models");
 
+// Route to GET all Quizzes
+router.get("/", async (req, res) => {
+	try {
+		const quizData = await Quizzes.findAll({
+			attributes: [],
+		});
+
+		// Serializing data so the template can read it
+		const quizzes = quizData.map((quiz) => quiz.get({ plain: true }));
+
+		// Passing serialized data into the Handlebars template
+		res.render("", {
+			quizzes,
+			on_quizPage: true,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 // Exports
 module.exports = router;
