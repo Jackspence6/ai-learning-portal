56aaad501d9019b20f9e0a6738df63680fcfa879
Jack Spence <jackspence123456@gmail.com>
2023-11-25 23:08:06 +0200

    Modified quiz rendering pages.

--- controllers/api/quizRoutes.js
+++ controllers/api/quizRoutes.js
@@ -47,7 +47,7 @@ router.get("/:id", async (req, res) => {
 
 		// Passing serialized data into the Handlebars template
 		res.render("individualQuiz", {
-			quizzes: quiz,
+			quiz,
 			logged_in: req.session.logged_in,
 			on_singleQuiz: true,
 		});
