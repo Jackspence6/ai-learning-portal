ba795ac0397f3f6ccb948e50fe1c06c270be47c4
Jack Spence <jackspence123456@gmail.com>
2023-11-27 16:36:12 +0200

    Added Route for user logout.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -59,5 +59,20 @@ router.post("/login", async (req, res) => {
 		res.status(400).json(err);
 	}
 });
+
+// Route to logout user if logged in
+router.get("/logout", (req, res) => {
+	// If user is logged in, destroying (deleting) their current session
+	if (req.session.logged_in) {
+		req.session.destroy(() => {
+			// Redirecting user to login page
+			res.redirect("/login");
+		});
+	} else {
+		// If user is not logged in, redirecting them to login page as well
+		res.redirect("/login");
+	}
+});
+
 // Exports
 module.exports = router;
